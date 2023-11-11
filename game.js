dd231a42bcf5aac3cda0f1057e389c3cf492221f
operezol <oriolperezolivares@gmail.com>
2023-11-12 00:02:19 +0100

    update

--- game.js
+++ game.js
@@ -1,29 +1,30 @@
 class MyGameScene extends Phaser.Scene {
-    constructor() {
-      super({ key: 'MyGameScene' });
-    }
-
-    preload() {
-      // Load any assets that your game needs here.
-    }
-
-    create() {
-      this.text = this.add.text(100, 100, 'Hello, world!');
-    }
+  constructor() {
+    super({ key: 'MyGameScene' });
   }
 
-  var game = new Phaser.Game({
-    width: "100%",
-    height: "100%",
-    type: Phaser.AUTO,
-    customEnvironment: false,
-    scale: {
-      mode: Phaser.Scale.FIT,
-      autoCenter: Phaser.Scale.CENTER_BOTH,
-      width: '100%',
-      height: '100%'
-    },
-    scene: MyGameScene});
+  preload() {
+    // Load any assets that your game needs here.
+  }
 
-  game.start();
+  create() {
+    this.text = this.add.text(100, 100, 'Hello, world!');
+  }
+}
+
+var game = new Phaser.Game({
+  width: "100%",
+  height: "100%",
+  type: Phaser.AUTO,
+  customEnvironment: false,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    width: '100%',
+    height: '100%'
+  },
+  scene: MyGameScene
+});
+
+game.start();
 
