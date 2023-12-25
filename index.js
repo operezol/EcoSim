827d764839dc13dd6299ca6aa51706629f4498a0
operezol <oriolperezolivares@gmail.com>
2023-12-25 18:53:12 +0100

    up

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ let widthUnit = 0;
 let heightUnit = 0;
 let canvasWidth = 0;
 let canvasHeight = 0;
-const canvasDivision = 200;
-const smoothDepthLevel = 2;
+const canvasDivision = 100;
+const smoothDepthLevel = 1;
 const smoothDepthRadius = 2;
-const rainLevel = 100;
+const rainLevel = 100000;
 const boardMap = [];
 
 function populateBoardMap() {
@@ -16,17 +16,6 @@ function populateBoardMap() {
       row.push({ depth: Math.floor(Math.random() * 256), waterLevel: 0 });
     }
     boardMap.push(row);
-  }
-}
-
-function smoothMap() {
-  for (let i = 0; i < boardMap.length; i++) {
-    for (let j = 0; j < boardMap[i].length; j++) {
-      const value = boardMap[i][j].depth;
-      const surroundingCoordinates = getSurroundingCoordinates(i, j);
-      const smoothedValue = calculateSmoothedDepth(surroundingCoordinates);
-      boardMap[i][j].depth = smoothedValue;
-    }
   }
 }
 
@@ -50,6 +39,16 @@ function calculateSmoothedDepth(surroundingCoordinates) {
   return Math.round(sum / surroundingCoordinates.length);
 }
 
+function smoothMapDepths() {
+  for (let i = 0; i < boardMap.length; i++) {
+    for (let j = 0; j < boardMap[i].length; j++) {
+      const surroundingCoordinates = getSurroundingCoordinates(i, j);
+      const smoothedDepth = calculateSmoothedDepth(surroundingCoordinates);
+      boardMap[i][j].depth = smoothedDepth;
+    }
+  }
+}
+
 const rain = () => {
   for (let i = 0; i < rainLevel; i++) {
     const firstNum = Math.floor(Math.random() * canvasDivision);
@@ -59,31 +58,50 @@ const rain = () => {
 };
 
 const waterFlow = () => {
-  for (let i = 0; i < boardMap.length; i++) {
-    for (let j = 0; j < boardMap[i].length; j++) {
-      const surroundingCoordinates = getSurroundingCoordinates(i, j);
+  for (let row = 0; row < boardMap.length; row++) {
+    for (let col = 0; col < boardMap[row].length; col++) {
+      const surroundingCoordinates = getSurroundingCoordinates(row, col);
       let waterSum = surroundingCoordinates.reduce(
-        (accumulator, obj) => accumulator + boardMap[obj.x][obj.y].waterLevel,
+        (sum, coords) => sum + boardMap[coords.x][coords.y].waterLevel,
         0
       );
+
       if (waterSum > 0) {
-        let surroundingPoints = surroundingCoordinates.map((coords) => ({
+        const surroundingPoints = surroundingCoordinates.map((coords) => ({
           x: coords.x,
           y: coords.y,
           depth: boardMap[coords.x][coords.y].depth,
           waterLevel: 0,
         }));
+
         surroundingPoints.sort((a, b) => a.depth - b.depth);
-        for (let index = 1; index < surroundingPoints.length; index++) {
-          let depthDiff = 0;
-          for (let pointer = index - 1; pointer >= 0; pointer--) {
-            // 
-            Acumular las diferencias de profundidad con los anteriores
-            depthDiff +=
-              surroundingPoints[pointer].depth +
-              surroundingPoints[pointer].waterLevel;
+
+        let totalWaterCapacity = 0;
+        let i = 1;
+
+        while (i < surroundingPoints.length && totalWaterCapacity <= waterSum) {
+          const higherDepth = surroundingPoints[i].depth;
+          const prevLevel =
+            surroundingPoints[i - 1].depth +
+            surroundingPoints[i - 1].waterLevel;
+          const capacityDiff = higherDepth - prevLevel;
+          totalWaterCapacity += capacityDiff;
+
+          if (totalWaterCapacity <= waterSum) {
+            surroundingPoints[i - 1].waterLevel += capacityDiff;
+            waterSum -= capacityDiff;
+          } else {
+            const equallyDividedWater = totalWaterCapacity / i;
+            surroundingPoints[i - 1].waterLevel += equallyDividedWater;
+            waterSum -= equallyDividedWater;
           }
+
+          i++;
         }
+
+        surroundingPoints.forEach((point) => {
+          boardMap[point.x][point.y].waterLevel = point.waterLevel;
+        });
       }
     }
   }
@@ -94,8 +112,8 @@ const renderMap = () => {
     for (let j = 0; j < boardMap[i].length; j++) {
       if (boardMap[i][j].waterLevel > 0) {
         fill(
-          boardMap[i][j].depth + boardMap[i][j].waterLevel,
-          boardMap[i][j].depth + boardMap[i][j].waterLevel,
+          Math.floor(boardMap[i][j].depth + boardMap[i][j].waterLevel),
+          Math.floor(boardMap[i][j].depth + boardMap[i][j].waterLevel),
           255
         );
       } else {
@@ -120,7 +138,7 @@ function setup() {
   createCanvas(canvasWidth, canvasHeight);
   populateBoardMap();
   for (let index = 0; index < smoothDepthLevel; index++) {
-    smoothMap();
+    smoothMapDepths();
   }
 }
 
